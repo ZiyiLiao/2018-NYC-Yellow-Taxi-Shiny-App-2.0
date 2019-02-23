cb9422fa449efdb8c58c38f898a62983fdcdd1c5
Liao Ziyi <liaoziyi@dyn-209-2-226-160.dyn.columbia.edu>
2019-02-23 16:26:52 -0500

    ui update2

--- app/zy/ui.R
+++ app/zy/ui.R
@@ -1,11 +1,3 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
 
 shinyUI(
   navbarPage("NYC Yellow Taxi", id = "navigation",
@@ -18,7 +10,10 @@ shinyUI(
                                   includeCSS("styles.css"),
                                   includeScript("gomap.js")
                                 ),
-                                
+                              
+                              #create a map
+                              leafletOutput('map', width = "100%", height = "100%") , 
+                              
                             fixedPanel(id = "controls", class = "panel panel-default", 
                                           draggable = T, top = 60, left = "auto", right = 5, bottom = "auto",
                                           width = 160, height = 130,
@@ -33,20 +28,13 @@ shinyUI(
                                         h3("Panel"),
                                         
                                         
-                                        selectInput("days", h4("Days"), c("All Day", "Business Day", "Not Business Day"),selected = "All Day"),
+                                        selectInput("days", h4("Days"), c("All Day", "Business Day", "Non-business Day"),selected = "All Day"),
 
                                         sliderInput(inputId = "hr_adjust", label = h4("Show Hour:"),
                                                     min = 0, max = 23, value = NULL, step = 1
                                                     # ,animate=animationOptions(interval = 500)
                                                    )
                                                   # ,helpText("Click play button to see dynamic flow data")
-                                        ,
-
-                                      # histogram ?  
-                                        plotOutput("districttimeplot", height = 280),
-                                        helpText(   a("Analysis",
-                                                      href="https://github.com/TZstatsADS/Spr2017-proj2-grp2/blob/master/doc/analysis.html")
-                                        )
                           )
           )
       )
