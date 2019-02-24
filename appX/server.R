076737021c7c632a72260e0ed69cc56b66210c7c
Shengjie Sun <ss5593@columbia.edu>
2019-02-24 11:38:58 -0500

    Update server.R

--- appX/server.R
+++ appX/server.R
@@ -1,25 +1,40 @@
-library(rgeos)
+library(shiny)
+library(dplyr)
+
 library(sp)
+library(rgeos)
 library(rgdal)
+
 library(leaflet)
 library(leaflet.extras)
-library(shiny)
+
 library(ggplot2)
-library(dplyr)
-library(data.table)
 library(RColorBrewer)
-library(sp)
-library(htmltools)
+
+
 
 setwd('../')
 
 # graphical parameters
-color = list(color1 = c('#F2D7D5','#D98880', '#CD6155', '#C0392B', '#922B21','#641E16'),
+color <- list(color1 = c('#F2D7D5','#D98880', '#CD6155', '#C0392B', '#922B21','#641E16'),
              color2 = c('#e6f5ff','#abdcff', '#70c4ff', '#0087e6', '#005998','#00365d'))
-bin = list(bin1 = c(0,100,1000,10000,100000,1000000,10000000), bin2 = c(0,0.05,0.1,0.15,0.2,0.25,9900))
-label = list(label1 = c("<100","100-1000","1000~10,000","10,000~100,000","100,000~1,000,000","1,000,000~10,000,000"),
+
+bin <- list(bin1 = c(0,100,1000,10000,100000,1000000,10000000), 
+           bin2 = c(0,0.05,0.1,0.15,0.2,0.25,9900))
+
+label <- list(label1 = c("<100","100-1000","1000~10,000","10,000~100,000","100,000~1,000,000","1,000,000~10,000,000"),
              label2 = c("0-0.05","0.05-0.1","0.1-0.15","0.15-0.2","0.2-0.25","0.25+"))
-title = list(t1 = "Pick Up Numbers", t2 = "Tip Percentage")
+
+title <- list(t1 = "Pick Up Numbers", 
+             t2 = "Tip Percentage")
+
+Background <- leaflet() %>% 
+  addSearchOSM() %>%
+  addResetMapButton() %>%
+  addTiles(group = "OSM") %>%
+  addProviderTiles("CartoDB", group="CartoDB")  %>%
+  setView(lat=40.7130, lng=-74.0059, zoom=11) %>%
+  addLayersControl(baseGroups=c("CartoDB", "OSM"))
 
 # load data
 Shape <- readOGR('data/taxi_zones', 'taxi_zones') %>% 
@@ -28,6 +43,8 @@ Shape <- readOGR('data/taxi_zones', 'taxi_zones') %>%
 Count <- readRDS('output/count_separated.rds')
 Tips <- readRDS('output/tips_separated.rds')
 Dropoff <- readRDS('output/dropoff_frequency.rds')
+
+
 
 # Server
 shinyServer(function(input, output,session) { 
@@ -50,33 +67,23 @@ shinyServer(function(input, output,session) {
     
     # calculate intermediate data
     if (input$days == "All Day"){
-      count_temp = Count %>% apply(c(1,2), sum)
-      tips_temp = Tips %>% apply(c(1,2), mean, na.rm = T)
+      count_temp <- Count %>% apply(c(1,2), sum)
+      tips_temp <- Tips %>% apply(c(1,2), mean, na.rm = T)
     }else{
-      count_temp = Count[ , , (input$days == "Business Day") + 1]
-      tips_temp = Tips[ , , (input$days == "Business Day") + 1]
+      count_temp <- Count[ , , (input$days == "Business Day") + 1]
+      tips_temp <- Tips[ , , (input$days == "Business Day") + 1]
     }
 
     if (!input$showhr){
-      Shape@data$count = count_temp %>% apply(1, sum)
-      Shape@data$tips = tips_temp %>% apply(1, mean, na.rm = T)
+      Shape@data$count <- count_temp %>% apply(1, sum)
+      Shape@data$tips <- tips_temp %>% apply(1, mean, na.rm = T)
     }else{
-      Shape@data$count = count_temp[, input$hr_adjust+1]
-      Shape@data$tips = tips_temp[, input$hr_adjust+1]
-      dropoff_temp = Dropoff %>% filter(busi == (input$days == "Business Day"),
-                                        hour == input$hr_adjust + 1)
+      Shape@data$count <- count_temp[, input$hr_adjust+1]
+      Shape@data$tips <- tips_temp[, input$hr_adjust+1]
     }
     
     # determine which layer to work with
     if (input$layers == "count"){
-      Background <- leaflet() %>% 
-        addSearchOSM() %>%
-        addResetMapButton() %>%
-        addTiles(group = "OSM") %>%
-        addProviderTiles("CartoDB", group="CartoDB")  %>%
-        setView(lat=40.7130, lng=-74.0059, zoom=11) %>%
-        addLayersControl(baseGroups=c("CartoDB", "OSM"))
-      
       pal1 = colorBin(color[[1]], bins = bin[[1]])
       
       Background %>%
@@ -93,14 +100,6 @@ shinyServer(function(input, output,session) {
       
     }
     else if (input$layers == "tips"){
-      Background <- leaflet() %>% 
-        addSearchOSM() %>%
-        addResetMapButton() %>%
-        addTiles(group = "OSM") %>%
-        addProviderTiles("CartoDB", group="CartoDB")  %>%
-        setView(lat=40.7130, lng=-74.0059, zoom=11) %>%
-        addLayersControl(baseGroups=c("CartoDB", "OSM"))
-      
       pal2 <- colorBin(color[[2]], bins = bin[[2]])
       
       Background %>%
@@ -122,15 +121,7 @@ shinyServer(function(input, output,session) {
   # create interactive map output
   output$Imap <- renderLeaflet({
     # create a basic layer that has no information
-    Background <- leaflet() %>% 
-      addSearchOSM() %>%
-      addResetMapButton() %>%
-      addTiles(group = "OSM") %>%
-      addProviderTiles("CartoDB", group="CartoDB")  %>%
-      setView(lat=40.7130, lng=-74.0059, zoom=11) %>%
-      addLayersControl(baseGroups=c("CartoDB", "OSM"))
-    
-    
+
     IBackground <- Background %>%
       addPolygons(data=Shape, weight=1, fillOpacity = .5,color = "grey",
                   highlightOptions = highlightOptions(weight = 3,
@@ -157,11 +148,24 @@ shinyServer(function(input, output,session) {
     
     # get the top_3 destination if pick up from the clicked polygon
     top3_info <- Dropoff %>% 
-      filter(busi == (days == 'Business Day'), hour == hr_adjusted,
-                     PULocationID == click_ID) %>% 
-      arrange(desc(n)) %>% top_n(3)
+      filter(busi == (days == 'Business Day'), 
+             hour == hr_adjusted,
+             PULocationID == click_ID) %>% 
+      arrange(desc(n)) %>% 
+      top_n(3) %>% 
+      filter(n > 0)
 
-    print(top3_info)
+    print(1)
+    # add top3_info to IBackground
+    
+    leafletProxy("Imap") %>%
+      clearGroup(group = "Once Plot") %>%
+      addPolygons(data = Shape[Shape$LocationID==top3_info$DOLocationID, ], 
+                  weight = 1, fillOpacity = .5,color = "red",
+                 highlightOptions = highlightOptions(weight = 3, color = "white", bringToFront = TRUE),
+                 group = "Once Plot"
+      )
+    print(2)
     
   })
   
